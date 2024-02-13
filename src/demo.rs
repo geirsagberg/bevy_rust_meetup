1219cf451caf71d6333c49f6a43fcbd609cc0d8c
Geir Sagberg <geir.sagberg@gmail.com>
2024-02-13 15:42:45 +0100

    Fresh demo

--- src/demo.rs
+++ src/demo.rs
@@ -3,36 +3,5 @@ use bevy::prelude::*;
 pub struct DemoPlugin;
 
 impl Plugin for DemoPlugin {
-    fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_stuff)
-            .add_systems(Update, move_stuff);
-    }
-}
-
-fn spawn_stuff(mut commands: Commands) {
-    commands.spawn(SpriteBundle {
-        sprite: Sprite {
-            custom_size: Some(Vec2::new(100.0, 100.0)),
-            color: Color::WHITE,
-            ..default()
-        },
-        ..default()
-    });
-}
-
-fn move_stuff(keyboard_input: Res<Input<KeyCode>>, mut query: Query<&mut Transform, With<Sprite>>) {
-    for mut transform in &mut query {
-        if keyboard_input.pressed(KeyCode::W) {
-            transform.translation.y += 10.0;
-        }
-        if keyboard_input.pressed(KeyCode::S) {
-            transform.translation.y -= 10.0;
-        }
-        if keyboard_input.pressed(KeyCode::A) {
-            transform.translation.x -= 10.0;
-        }
-        if keyboard_input.pressed(KeyCode::D) {
-            transform.translation.x += 10.0;
-        }
-    }
+    fn build(&self, app: &mut App) {}
 }
